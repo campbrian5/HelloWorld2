ce30c0db78fbb9042c8e6a94d00a85afec7911b5
brian campbell <brian.campbell@maine.edu>
2020-01-29 10:50:43 -0500

    firstBCommit

--- gitHello/driver.java
+++ gitHello/driver.java
@@ -3,6 +3,7 @@ package gitHello;
 public class driver {
 
 	public static void main (String args[]) {
+		System.out.println("Here ye: important announcement");
 		System.out.println("Hello World");
 	}
 }
