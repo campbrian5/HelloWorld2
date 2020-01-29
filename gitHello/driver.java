37e3efdf96cac29021e57e95fd328a18af725687
brian campbell <brian.campbell@maine.edu>
2020-01-29 10:52:37 -0500

    secondBCommit

--- gitHello/driver.java
+++ gitHello/driver.java
@@ -5,5 +5,6 @@ public class driver {
 	public static void main (String args[]) {
 		System.out.println("Here ye: important announcement");
 		System.out.println("Hello World");
+		System.out.println("Goodbye, cruel world.");
 	}
 }
